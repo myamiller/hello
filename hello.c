faf6c9d21c9ae8f3f3c58ec87ec17df4ea5b7536
Isaac Young <iyoung4@huskers.unl.edu>
2020-08-20 20:11:16 +0000

    added collaborator name and major to output

--- hello.c
+++ hello.c
@@ -10,6 +10,9 @@
 
  int main (int argc, char **argv) {
      printf ("Mya Miller!\n");
+     printf("Undeclared!\n");
+     printf("Isaac Young!\n");
+     printf("Actuarial Science!\n");
 
      return 0;
  }
